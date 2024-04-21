d08a7dd6adb518c19a6e901bb2eb8b9036f3ee7f
maro-12 <naoya.kurahashi@gmail.com>
2024-04-21 20:07:13 +0900

    開始・完了それぞれのプロパティを入力できるようにした

--- scripts/options.js
+++ scripts/options.js
@@ -1,23 +1,43 @@
 $(document).ready(() => {
-  $("#statusPropertyId").select2();
-  $("#statusOptionId").select2();
-  $("#datePropertyId").select2();
+  $("#startStatusPropertyId").select2();
+  $("#startStatusOptionId").select2();
+  $("#startDatePropertyId").select2();
+  $("#completeStatusPropertyId").select2();
+  $("#completeStatusOptionId").select2();
+  $("#completeDatePropertyId").select2();
+
+  {
+    token,
+      databaseId,
+      startStatusPropertyId,
+      startStatusOptionId,
+      startDatePropertyId,
+      completeStatusPropertyId,
+      completeStatusOptionId,
+      completeDatePropertyId;
+  }
 
   // 設定をフォームに読み込む
   chrome.storage.sync.get(
     [
       "token",
       "databaseId",
-      "statusPropertyId",
-      "statusOptionId",
-      "datePropertyId",
+      "startStatusPropertyId",
+      "startStatusOptionId",
+      "startDatePropertyId",
+      "completeStatusPropertyId",
+      "completeStatusOptionId",
+      "completeDatePropertyId",
     ],
     (items) => {
       $("#token").val(items.token);
       $("#databaseId").val(items.databaseId);
-      $("#statusPropertyId").val(items.status);
-      $("#statusOptionId").val(items.status);
-      $("#datePropertyId").val(items.date);
+      $("#startStatusPropertyId").val(items.startStatusPropertyId);
+      $("#startStatusOptionId").val(items.startStatusOptionId);
+      $("#startDatePropertyId").val(items.startDatePropertyId);
+      $("#completeStatusPropertyId").val(items.completeStatusPropertyId);
+      $("#completeStatusOptionId").val(items.completeStatusOptionId);
+      $("#completeDatePropertyId").val(items.completeDatePropertyId);
     }
   );
 
@@ -45,39 +65,62 @@ $(document).ready(() => {
       }
     );
     databaseRes = await result.json();
+    initSelect();
+  });
 
-    if (databaseRes.statusProperties.length > 0) {
-      const statusOptions = databaseRes.statusProperties.map((_) => ({
+  const initSelect = () => {
+    const statusProperties = databaseRes.statusProperties;
+    if (statusProperties.length > 0) {
+      const statusOptions = statusProperties.map((_) => ({
         id: _.id,
         text: _.name,
       }));
-      $("#statusPropertyId").select2({ data: statusOptions });
+      $("#startStatusPropertyId").select2({ data: statusOptions });
+      $("#completeStatusPropertyId").select2({ data: statusOptions });
 
-      const options = databaseRes.statusProperties[0].completeGroup.options.map(
-        (_) => ({
+      const inProgressGroupOptions =
+        statusProperties[0].inProgressGroup.options.map((_) => ({
           id: _.id,
           text: _.name,
-        })
-      );
-      $("#statusOptionId").select2({ data: options });
+        }));
+      $("#startStatusOptionId").select2({ data: inProgressGroupOptions });
+      const completeGroupOptions =
+        statusProperties[0].completeGroup.options.map((_) => ({
+          id: _.id,
+          text: _.name,
+        }));
+      $("#completeStatusOptionId").select2({ data: completeGroupOptions });
     }
     const dateOptions = databaseRes.dateProperties.map((_) => ({
       id: _.id,
       text: _.name,
     }));
-    $("#datePropertyId").select2({ data: dateOptions });
+    $("#startDatePropertyId").select2({ data: dateOptions });
+    $("#completeDatePropertyId").select2({ data: dateOptions });
+  };
+
+  // ステータスプロパティの変更に応じて、オプションプロパティの選択肢を更新する
+  $("#startStatusPropertyId").on("select2:select", (e) => {
+    const data = e.params.data;
+    const newOptions = databaseRes.statusProperties
+      .filter((_) => _.id === data.id)[0]
+      .inProgressGroup.options.map((_) => ({ id: _.id, text: _.name }));
+
+    // 古いオプションが残り続けないように、一度全て削除する
+    $("#startStatusOptionId").children().remove();
+    $("#startStatusOptionId").select2({ data: newOptions });
   });
 
   // ステータスプロパティの変更に応じて、オプションプロパティの選択肢を更新する
-  $("#statusPropertyId").on("select2:select", (e) => {
+  $("#completeStatusPropertyId").on("select2:select", (e) => {
     const data = e.params.data;
     const newOptions = databaseRes.statusProperties
       .filter((_) => _.id === data.id)[0]
       .completeGroup.options.map((_) => ({ id: _.id, text: _.name }));
 
     // 古いオプションが残り続けないように、一度全て削除する
-    $("#statusOptionId").children().remove();
-    $("#statusOptionId").select2({ data: newOptions });
+    $("#completeStatusOptionId").children().remove();
+    $("#completeStatusOptionId").select2({ data: newOptions });
   });
 
   // フォームのサブミットをハンドル
@@ -87,18 +130,24 @@ $(document).ready(() => {
     // 入力値を取得
     var token = $("#token").val();
     var databaseId = $("#databaseId").val();
-    var statusPropertyId = $("#statusPropertyId").val();
-    var statusOptionId = $("#statusOptionId").val();
-    var datePropertyId = $("#datePropertyId").val();
+    var startStatusPropertyId = $("#startStatusPropertyId").val();
+    var startStatusOptionId = $("#startStatusOptionId").val();
+    var startDatePropertyId = $("#startDatePropertyId").val();
+    var completeStatusPropertyId = $("#completeStatusPropertyId").val();
+    var completeStatusOptionId = $("#completeStatusOptionId").val();
+    var completeDatePropertyId = $("#completeDatePropertyId").val();
 
     // 設定を保存
     chrome.storage.sync.set(
       {
         token,
         databaseId,
-        statusPropertyId,
-        statusOptionId,
-        datePropertyId,
+        startStatusPropertyId,
+        startStatusOptionId,
+        startDatePropertyId,
+        completeStatusPropertyId,
+        completeStatusOptionId,
+        completeDatePropertyId,
       },
       () => {
         // 保存が完了したら、何らかのフィードバックをユーザーに提供する
